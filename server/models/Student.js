a5d1e3d7c84a882bb421d537b0ed16093b4fe496
shreyash talole <shreyastalole2001@gmail.com>
2022-08-02 22:58:19 +0530

    mongoDB connection created

---
+++ server/models/Student.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const studentSchema = new Schema({
+
+    name: {
+        type: String,
+        required : true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+
+})
+
+const Student = mongoose.model('Student', studentSchema)
+module.exports = Student
